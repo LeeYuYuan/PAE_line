5dd358c0fbaeb00ff1b43b2898143cc3e8b1892d
Berry Lee <you@example.com>
2019-06-10 19:22:22 +0800

    2019/6/10

--- app.py
+++ app.py
@@ -42,10 +42,17 @@ def handle_message(event):
 
     if msg in ['hi', 'Hi', '你好']:
         r = '你好~今天運動沒'
-        if msg in ['還沒', '等等就去', '今天有點懶', '不想運動', '明天再去']:
-            r = '不要偷懶啦!!!!快去運動吧'
+    elif msg in ['還沒', '等等就去', '今天有點懶', '不想運動', '明天再去']:
+        r = '不要偷懶啦!!!!快去運動吧'
     elif msg in ['我想運動', '好想運動啊!', '好想運動']:
         r = '快來看看新的運動資訊' + '\n' + 'https://paendless.wixsite.com/mysite/blog'
+    elif msg in ['怎麼聯絡你們?', '我想找客服', '我要投訴']:
+        r = '點我~' + '\n' + 'https://paendless.wixsite.com/mysite/blank-1'
+    elif msg in ['我想成為分享者']:
+        r = '點我~' + '\n' + 'https://paendless.wixsite.com/mysite/writer'
+    elif msg in ['我今天心情不好', '陪我聊天', '覺得無聊']:
+        r = '想聊點什麼嗎~'
+
     
     line_bot_api.reply_message(
         event.reply_token,
