6cb611cbd472873dfe3ccae5aeab97db7a641610
Berry Lee <you@example.com>
2019-06-11 12:39:54 +0800

    sticker

--- app.py
+++ app.py
@@ -49,7 +49,8 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             sticker_message) 
-
+        
+        msg = event.message.text
         r = '快去運動' 
         line_bot_api.reply_message(
             event.reply_token,
