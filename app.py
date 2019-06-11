3a3dcdebf87cb0fd1badd7356e23d4f067e1ff56
Berry Lee <you@example.com>
2019-06-11 12:53:16 +0800

    sticker

--- app.py
+++ app.py
@@ -52,12 +52,12 @@ def handle_message(event):
 
         return
 
-
+    count = 0
     if msg in ['hi', 'Hi', '你好']:
         r = '你好~今天運動沒'
 
     elif msg in ['還沒', '等等就去', '今天有點懶', '不想運動', '明天再去']:
-        
+        count = count + 1
         sticker_message = StickerSendMessage(
             package_id='11539',
             sticker_id='52114142'
@@ -66,8 +66,9 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             sticker_message) 
-        r = '不要偷懶啦!!!!快去運動吧'
-        
+        if count == 3:
+            r = '不要偷懶啦!!!!快去運動吧'
+
     elif msg in ['我想運動', '好想運動啊!', '好想運動']:
         r = '快來看看新的運動資訊' + '\n' + 'https://paendless.wixsite.com/mysite/blog'
     elif msg in ['怎麼聯絡你們?', '我想找客服', '我要投訴']:
